dd28eae99eb3c8f0baff764e1cbb3138c4a635b0
An6666666 <andy5888123123@gmail.com>
2024-09-20 16:02:00 +0800

    1

--- bmi.js
+++ bmi.js
@@ -1,5 +1,7 @@
-var a=50;
-var b=160;
+function BMI() {
+var a = Number(document.getElementById("a").value);
+var b = Number(document.getElementById("b").value);
 var bmi=a/((b/100)**2);
-
+document.getElementById("bmi").innerHTML = bmi;
 console.log("Hello"+bmi);
+}
