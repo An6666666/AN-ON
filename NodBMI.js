8723305a4d67dcc33daa0fc1e7e6939e0661844e
An6666666 <andy5888123123@gmail.com>
2024-09-27 15:58:22 +0800

    a

--- NodBMI.js
+++ NodBMI.js
@@ -1,6 +1,18 @@
 var readline = require("readline-sync");
 
-var a = readline. question("Please input ur weight(KG)?");
-var b = 160;
+var a = readline. questionInt("Please input ur weight(KG)?");
+var b = readline. questionInt("Please input ur hight(CM)?");
 var bmi=a/((b/100)**2);
-console.log("Hello"+bmi);
+if (bmi < 18.5){
+    greeting="過輕";
+}
+else if(18.5 <= bmi && bmi < 24){
+    greeting="健康";
+}
+else if(24 <= bmi && bmi < 27){
+    greeting="過重";
+}
+else {
+    greeting="肥胖";
+}
+console.log(greeting+bmi);
