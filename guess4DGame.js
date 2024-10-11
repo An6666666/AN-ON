fdcb0e85fd43720a4711e5a036e756df1e994154
An6666666 <andy5888123123@gmail.com>
2024-10-11 13:30:32 +0800

    1

--- guess4DGame.js
+++ guess4DGame.js
@@ -13,10 +13,34 @@ do{
   var G=readline.questionInt("Please input 4 digits? ");
 }while(G<1000 || G>=10000);
 var Gstr = G.toString();
-//adjuge ?A?B
-var countA=0,countB=0;
-for(){//Ans[0]~A[3]
-   for(){//Gstr[0]!Gstr[3]
+var countA = 0, countB = 0;
+    var checkedAns = new Array(4).fill(false);
+    var checkedG = new Array(4).fill(false);
 
-   }
-}
+    for (var i = 0; i < 4; i++) {
+        if (Gstr[i] === AnsStr[i]) {
+            countA++;
+            checkedAns[i] = true;
+            checkedG[i] = true;
+        }
+    }
+
+    for (var i = 0; i < 4; i++) {
+        if (!checkedG[i]) {
+            for (var j = 0; j < 4; j++) {
+                if (!checkedAns[j] && Gstr[i] === AnsStr[j]) {
+                    countB++;
+                    checkedAns[j] = true; // 标记这个数字为已检查
+                    break; // 退出内层循环
+                }
+            }
+        }
+    }
+
+    console.log(`${countA}A ${countB}B`);
+
+    // 检查猜测是否正确
+    if (countA === 4) {
+        correct = true;
+        console.log(`恭喜你！你在${attempts}次尝试中猜到了数字 ${AnsStr}。`);
+    }
