4991afe409e109d96f6b95e39d4be06ba2b59f20
An6666666 <andy5888123123@gmail.com>
2024-11-01 15:05:32 +0800

    1

--- GameLife/gamelife.js
+++ GameLife/gamelife.js
@@ -2,68 +2,55 @@
 const Live = 1;
 const Dead = 0;
 
-class Life{
-   constructor(_row, _col){
+class Life {
+    constructor(_row, _col) {
         this.row = _row;
         this.col = _col;
-        this.grid=[];//new Array()
-        //2d array
-        for(var _row=0;_row < this.row;_row++){
-            this.grid.push([]);
-            for(var _col=0;_col < this.col;_col++){
-                this.grid[_row].push(Dead);
-            }
-        }
-        
-    }
-    initialize = function(){
-        this.grid[1][1] = Live;
-        this.grid[1][2] = this.grid[1][3] = this.grid[1][4] =Live; 
+        this.grid = Array.from({ length: this.row }, () => Array(this.col).fill(Dead));
     }
 
-    update = function(){
-          var nextGrid = JSON.parse(JSON.stringify(this.grid));
-          //travse all elements, count its neighbor
-          var neighbor;
-          for (let _row = 0; _row < this.row; _row++) {
-            for (let _col = 0; _col < this.row; _col++) {
-                neighbor = this.neighborCount(_row, _col);
-                // update by 4 rules
-                if(this.getStatusAt(_row,_col)==Live && (neighbor<=1 || neighbor>=4)){
+    initialize() {
+        this.grid[1][1] = Live;
+        this.grid[1][2] = this.grid[1][3] = this.grid[1][4] = Live; 
+    }
+
+    update() {
+        let nextGrid = JSON.parse(JSON.stringify(this.grid));
+        for (let _row = 0; _row < this.row; _row++) {
+            for (let _col = 0; _col < this.col; _col++) {
+                const neighbor = this.neighborCount(_row, _col);
+                const status = this.getStatusAt(_row, _col);
+                
+                if (status === Live && (neighbor < 2 || neighbor > 3)) {
                     nextGrid[_row][_col] = Dead;
                 }
-                if(this.getStatusAt(_row,_col)==Dead && neighbor==3){
+                if (status === Dead && neighbor === 3) {
                     nextGrid[_row][_col] = Live;
                 }
-
             }
-            
-          }
-
-          this.grid = null;
-          this.grid = nextGrid;
-    } 
-
-
-    neighborCount = function(row, col){
-        var count=0;
-        count += this.getStatusAt(row-1, col-1);
-        count += this.getStatusAt(row-1, col); 
-        count += this.getStatusAt(row-1, col+1); 
-        count += this.getStatusAt(row, col-1);
-        count += this.getStatusAt(row, col+1); 
-        count += this.getStatusAt(row+1, col-1); 
-        count += this.getStatusAt(row+1, col); 
-        count += this.getStatusAt(row+1, col+1);      
-        return count;   
+        }
+        this.grid = nextGrid;
     }
-    getStatusAt = function(row, col){
-        if(row<0 || col<0 || row >= this.row || col >= this.col){
-           return Dead;
-        }else{
+
+    neighborCount(row, col) {
+        let count = 0;
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                if (i === 0 && j === 0) continue; // skip the cell itself
+                count += this.getStatusAt(row + i, col + j);
+            }
+        }
+        return count;
+    }
+
+    getStatusAt(row, col) {
+        if (row < 0 || col < 0 || row >= this.row || col >= this.col) {
+            return Dead;
+        } else {
             return this.grid[row][col];
         }
     }
+
     draw() {
         console.clear(); // 清除控制台
         for (let row of this.grid) {
@@ -72,13 +59,12 @@ class Life{
     }
 }
 
-// Life.prototype.update= function(){
-
-// }
-
-var myGame = new Life(100,100);
-var myGame2 = new Life(100,100);
-
+// 創建生命遊戲實例
+const myGame = new Life(10, 10);
 myGame.initialize();
-//myGame.draw()
-myGame.update();
+
+// 主迴圈，每秒更新並繪製
+setInterval(() => {
+    myGame.update();
+    myGame.draw();
+}, 1000); // 每秒更新一次
