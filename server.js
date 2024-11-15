8666145fc85157a21c17ab61629d98f933f190f6
An6666666 <andy5888123123@gmail.com>
2024-11-15 13:54:13 +0800

    1

--- server.js
+++ server.js
@@ -3,20 +3,29 @@
 var express = require("express");
 var server = express();
 
-var DB = require("nedb-promises"); 
-var Profolio = DB.create(__dirname+"/profolio.db");  
-Profolio.insert({modal: "#portfolioModal1", imgSrc:"roundicons.png", heading:"Round Icons", text:"Graphic Design"})
-
 //web root
 server.use(express.static(__dirname+"/AgencyProject"));
 
-server.get("/profolio", (req,res)=>{
-    Services:[
+var DB = require("nedb-promises"); 
+var Profolio = DB.create(__dirname+"/profolio.db");
+
+Profolio.insert({modal: "#portfolioModal1", imgSrc:"roundicons.png", heading:"Round Icons", text:"Graphic Design"})
+
+server.get("/services", (req, res)=>{
+    //DB find
+    var Services=[
         {icon: "fa-shopping-cart", heading:"E-Commerce", text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."},
         {icon: "fa-laptop", heading:"Responsive Design", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."}
-    ]
-    res.send("Profolio")
-    })
+    ];
+    res.send(Services);
+});
+
+server.get("/profolio", (req,res)=>{
+    //DB
+
+    res.send("Profolio");
+
+})
 
 server.listen(80, ()=>{
     console.log("Server is running at port 80.");
