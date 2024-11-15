7a734022e5ee9cbf55161472cbe4f79aadf1e4c3
An6666666 <andy5888123123@gmail.com>
2024-11-15 16:11:47 +0800

    1

--- 1115.js
+++ 1115.js
@@ -1,20 +1,36 @@
-function  bracketMatch(inputString){
-    var opening=[];
-    var isMatched = true;
-    var symbol = inputString.charAt(i=0);
-   while(isMatched && symbol != '\n'){
-       if(symbol == '{' || symbol =='(' || symbol =='[')
-         opening.push(symbol);
-       if(symbol== '}' || symbol ==')' || symbol ==']'){
-       if(opening.length==0) isMatched=false;
-       else{
-             match = opening.pop();
-             isMatched = (symbol == '}' && match=='{') || (symbol == ')' && match=='(') ||
-                         (symbol == ']' && match=='[');
-       }
+function  bracketMatch (inputString){
+  var opening=[];
+  var isMatched = true;
+  var Left = "{([<";
+  var Right = "})]>";
+  var i=0;
+  var symbol = inputString.charAt(i);
+ while(isMatched && symbol != ' '){
+     //if(symbol == '{' || symbol =='(' || symbol =='['){
+       //opening.push(symbol);
+    // }
+    if(Left.includes(symbol)){
+      opening.push(symbol);
+    }
+     if(Right.includes(symbol)){
+      if(opening.length==0){
+      isMatched=false;
+      }
+      else{
+           var match = opening.pop();
+           isMatched = (symbol == '}' && match=='{') || 
+                       (symbol == ')' && match=='(') || 
+                       (symbol == ']' && match=='[');
+      }
      }
-      symbol = inputString.charAt(++i);
-   }
-   if(opening.length>0 || !isMatched) return 'unmatched';
-   else  return 'matched';
+    symbol = inputString.charAt(++i);
  }
+ if(opening.length>0 || !isMatched){ 
+  return 'unmatched';
+ }else{
+  return 'matched';
+ }
+}
+
+console.log(bracketMatch("<{4[8(0)2]}>"));
+
